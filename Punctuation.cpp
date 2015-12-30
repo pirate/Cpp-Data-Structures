5f8134384d865a0f7f3e1e7b6ef32087ea5f69b7
Nick Sweeting <git@nicksweeting.com>
2015-12-29 17:10:11 -0800

    Update Punctuation.cpp

--- Punctuation.cpp
+++ Punctuation.cpp
@@ -14,20 +14,15 @@ using namespace std;
 
 bool ispunct(char q) {
     char punct[26] = ".!-_? <>,*&^$!;:'()[]{}|`";
-    int p;
-    for (p=0;p<25;p++) {
-        if (q == punct[p]) {
-            return true;
-        }
+    for (int p=0;p<25;p++) {
+        if (q == punct[p]) return true;
     }
     return false;
 }
 
 int main() {
-    if (ispunct(' ')) {
+    if (ispunct(' '))
         cout << "its punct" << endl;
-    }
-    else {
+    else
         cout << "not punct." << endl;
-    }
 }
